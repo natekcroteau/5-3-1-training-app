fba197dec62abe7149a9df241ebb38cdb8d3a651
natekcroteau <natekcroteau@gmail.com>
2021-08-18 09:46:14 -0600

    setup skeleton functions for calculating weekly weight loads

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import './App.css';
 
 function App() {
+
+  function weekOne(max){
+    
+  }
+
+  function weekTwo(max){
+
+  }
+
+  function weekThree(max){
+
+  }
+
+  function weekFour(max){
+
+  }
+
   return (
     <div className="App">
       
