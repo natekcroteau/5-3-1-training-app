8327db88d4a99256da52746914e233beb3226a71
natekcroteau <natekcroteau@gmail.com>
2021-08-18 09:56:21 -0600

    added logic to calculate percentages for each training cycle

--- src/App.js
+++ src/App.js
@@ -3,19 +3,43 @@ import './App.css';
 function App() {
 
   function weekOne(max){
+    let weekOneTargets = []
+
+    weekOneTargets.push(Math.round(max*0.65))
+    weekOneTargets.push(Math.round(max*0.75))
+    weekOneTargets.push(Math.round(max*0.85))
     
+    return weekOneTargets
   }
 
   function weekTwo(max){
+    let weekTwoTargets = []
 
+    weekTwoTargets.push(Math.round(max*0.70))
+    weekTwoTargets.push(Math.round(max*0.80))
+    weekTwoTargets.push(Math.round(max*0.90))
+    
+    return weekTwoTargets
   }
 
   function weekThree(max){
+    let weekThreeTargets = []
 
+    weekThreeTargets.push(Math.round(max*0.75))
+    weekThreeTargets.push(Math.round(max*0.85))
+    weekThreeTargets.push(Math.round(max*0.95))
+    
+    return weekThreeTargets
   }
 
   function weekFour(max){
+    let weekFourTargets = []
 
+    weekFourTargets.push(Math.round(max*0.40))
+    weekFourTargets.push(Math.round(max*0.50))
+    weekFourTargets.push(Math.round(max*0.60))
+    
+    return weekFourTargets
   }
 
   return (
