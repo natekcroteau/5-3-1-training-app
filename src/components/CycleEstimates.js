034d99a13c5b2be5fab907dd41b42a332bddfc0a
natekcroteau <natekcroteau@gmail.com>
2021-08-26 17:29:52 -0600

    adjusted styling and added input for future log submission of cycle calculations

--- src/components/CycleEstimates.js
+++ src/components/CycleEstimates.js
@@ -8,9 +8,19 @@ export default function CycleEstimates(props){
     const { roundForPlates } = props
 
     const [maxWeight, setMaxWeight] = useState(0)
+    const [lift, setLift] = useState("ie: Deadlift")
 
     const handleMaxChange = (event) => {
         setMaxWeight(event.target.value)
+    }
+
+    const handleLiftChange = (event) => {
+        setLift(event.target.value)
+    }
+
+    const handleCycleSubmission = (event) => {
+        event.preventDefault()
+
     }
 
     function weekOne(max){
@@ -55,33 +65,40 @@ export default function CycleEstimates(props){
 
     return (
         <div className="cycle-calculations">
-        <h2>Calculate 5/3/1 Training Loads</h2>
-        <label>Current 1RM</label>
-        <input type="text" value={maxWeight} onChange={handleMaxChange} />
-        <h4>Week One:</h4>
-        <div className="cycle-week">
-            <div>Set 1: {weekOne(maxWeight)[0]} lbs x 5 reps</div>
-            <div>Set 2: {weekOne(maxWeight)[1]} lbs x 5 reps</div>
-            <div>Set 3: {weekOne(maxWeight)[2]} lbs x 5+ reps</div>
+            <h2>Calculate 5/3/1 Training Loads</h2>
+            <label>Current 1RM</label>
+            <input type="text" value={maxWeight} onChange={handleMaxChange} />
+            <div className="cycle-month">
+                <div className="cycle-week">
+                <h4>Week One:</h4>
+                    <div>Set 1: {weekOne(maxWeight)[0]} lbs x 5 reps</div>
+                    <div>Set 2: {weekOne(maxWeight)[1]} lbs x 5 reps</div>
+                    <div>Set 3: {weekOne(maxWeight)[2]} lbs x 5+ reps</div>
+                </div>
+                <div className="cycle-week">
+                <h4>Week Two:</h4>
+                    <div>Set 1: {weekTwo(maxWeight)[0]} lbs x 3 reps</div>
+                    <div>Set 2: {weekTwo(maxWeight)[1]} lbs x 3 reps</div>
+                    <div>Set 3: {weekTwo(maxWeight)[2]} lbs x 3+ reps</div>
+                </div>
+                <div className="cycle-week">
+                <h4>Week Three:</h4>
+                    <div>Set 1: {weekThree(maxWeight)[0]} lbs x 5 reps</div>
+                    <div>Set 2: {weekThree(maxWeight)[1]} lbs x 3 reps</div>
+                    <div>Set 3: {weekThree(maxWeight)[2]} lbs x 1+ reps</div>
+                </div>
+                <div className="cycle-week">
+                <h4>Week Four:</h4>
+                    <div>Set 1: {weekFour(maxWeight)[0]} lbs x 5 reps</div>
+                    <div>Set 2: {weekFour(maxWeight)[1]} lbs x 5 reps</div>
+                    <div>Set 3: {weekFour(maxWeight)[2]} lbs x 5 reps</div>
+                </div>
+            </div>
+            <form onSubmit={handleCycleSubmission} >
+                <label>Which Lift? </label>
+                <input type="text" value={lift} onChange={handleLiftChange} />
+                <input type="submit" value="Save To Log"/>
+            </form>
         </div>
-        <h4>Week Two:</h4>
-        <div className="cycle-week">
-            <div>Set 1: {weekTwo(maxWeight)[0]} lbs x 3 reps</div>
-            <div>Set 2: {weekTwo(maxWeight)[1]} lbs x 3 reps</div>
-            <div>Set 3: {weekTwo(maxWeight)[2]} lbs x 3+ reps</div>
-        </div>
-        <h4>Week Three:</h4>
-        <div className="cycle-week">
-            <div>Set 1: {weekThree(maxWeight)[0]} lbs x 5 reps</div>
-            <div>Set 2: {weekThree(maxWeight)[1]} lbs x 3 reps</div>
-            <div>Set 3: {weekThree(maxWeight)[2]} lbs x 1+ reps</div>
-        </div>
-        <h4>Week Four:</h4>
-        <div className="cycle-week">
-            <div>Set 1: {weekFour(maxWeight)[0]} lbs x 5 reps</div>
-            <div>Set 2: {weekFour(maxWeight)[1]} lbs x 5 reps</div>
-            <div>Set 3: {weekFour(maxWeight)[2]} lbs x 5 reps</div>
-        </div>
-      </div>
     )
 }
