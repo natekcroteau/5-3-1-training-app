b96ae8864d8256b6088cd6767b64ab1e5821ca9b
natekcroteau <natekcroteau@gmail.com>
2021-08-31 20:20:21 -0600

    updated with manual authentication methods for login and account creation

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,69 +1,59 @@
 import { useState } from 'react'
 import '../App.css'
 import { GiWeightLiftingUp } from 'react-icons/gi'
-// import { useDispatch, useSelector } from 'react-redux'
-import { initializeApp } from "firebase/app"
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { useDispatch, useSelector } from 'react-redux'
+
 
 
 export default function Account(){
 
+    const dispatch = useDispatch()
 
-    const [email, setEmail] = useState('')
+
+    const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
 
 
-    const firebaseConfig = {
-        apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-        authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-        storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.REACT_APP_FIREBASE_APP_ID
-    }
-
-    const app = initializeApp(firebaseConfig)
-
-
-    const auth = getAuth();
-    createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-        // Signed in 
-        const user = userCredential.user;
-        // set redux user
-        console.log(user)
-    })
-    .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorCode, errorMessage)
-    })
-
-
-    function handleEmailChange(event){
-        setEmail(event.target.value)
+    function handleUsernameChange(event){
+        setUsername(event.target.value)
     }
     
     function handlePasswordChange(event){
         setPassword(event.target.value)
     }
 
-    function handleLogin(){
-       
+    function handleLogin(event){
+        event.preventDefault()
+        fetch('http://localhost:3001/login', {
+            method: 'POST',
+            headers: {
+                "Content-Type" : "application/json"
+            },
+            body: JSON.stringify({ user: {"username": username, "password": password}})
+            .then(results => {
+                dispatch({type: "SET_USER", user: results.user[1].username })
+                localStorage.setItem("token", results.token)
+            })
+        })
     }
 
     function handleAccountCreation(event){
         event.preventDefault()
-        createUserWithEmailAndPassword(auth, email, password)
-        // useDispatch({type: "SET_USER", user: user })
+        fetch('http://localhost:3001/users', { 
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({user: {"username": username, "password": password}})
+        })
     }
 
     return(
         <>
             <GiWeightLiftingUp className="account-icon" />
             <form className="account">
-                <label>Email</label>
-                <input type="text" onChange={handleEmailChange} value={email}/>
+                <label>Username</label>
+                <input type="text" onChange={handleUsernameChange} value={username}/>
                 <label>Password</label>
                 <input type="password" onChange={handlePasswordChange} value={password}/>
                 <button onClick={handleLogin} >Login</button>
